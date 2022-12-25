5993526d5f091194db7a6b61dcfd4fc87af7de9e
Dominik Pietrak <dominip89@mi.fu-berlin.de>
2022-12-26 00:29:28 +0100

    Added install scripts for windows and ubuntu

---
+++ install_script.ps1
@@ -0,0 +1,56 @@
+#Install WinGet
+#Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
+$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
+    "Installing winget Dependencies"
+    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri $releases_url
+    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+
+    "Installing winget from $($latestRelease.browser_download_url)"
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+else {
+    "winget already installed"
+}
+
+Write-Output "Installing Apps"
+$apps = @(
+    @{name = "Microsoft.WindowsTerminal"; source = "msstore" }, 
+    @{name = "Microsoft.PowerToys" }, 
+    @{name = "Microsoft.VisualStudioCode" }, 
+    @{name = "Mozilla.Firefox"},
+    @{name = "Discord.Discord"},
+    @{name = "OBSProject.OBSStudio"},
+    @{name = "Valve.Steam"},
+);
+
+Foreach ($app in $apps) {
+    $listApp = winget list --exact -q $app.name
+    if (![String]::Join("", $listApp).Contains($app.name)) {
+        Write-host "Installing:" $app.name
+        if ($app.source -ne $null) {
+            winget install --exact --silent $app.name --source $app.source
+        }
+        else {
+            winget install --exact --silent $app.name 
+        }
+    }
+    else {
+        Write-host "Skipping Install of " $app.name
+    }
+}
+
+#Remove Apps
+Write-Output "Removing Apps"
+
+$apps = "*3DPrint*", "Microsoft.MixedReality.Portal"
+Foreach ($app in $apps)
+{
+  Write-host "Uninstalling:" $app
+  Get-AppxPackage -allusers $app | Remove-AppxPackage
+}
---
+++ install_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3 python-is-python3 default-jre default-jdk texlive-full pandoc
