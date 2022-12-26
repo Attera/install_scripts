6983830ec767bf397bacfdfa790fbc58430bf644
Dominik Pietrak <dominip89@mi.fu-berlin.de>
2022-12-26 01:16:07 +0100

    Added a debloater for fresh windows

---
+++ debloater.ps1
@@ -0,0 +1,118 @@
+# Based on https://github.com/Kugane/winget/blob/main/winget-basic.ps1
+
+$bloatware = @(
+    # default Windows 11 apps
+    "MicrosoftTeams"
+    "Microsoft.Todos"
+    "Microsoft.PowerAutomateDesktop"
+
+    # default Windows 10 apps
+    "Microsoft.549981C3F5F10"        # Cortana Offline
+    "Microsoft.OneDriveSync"         # Onedrive
+    "Microsoft.3DBuilder"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingTranslator"
+    "Microsoft.BingWeather"
+    "Microsoft.FreshPaint"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftPowerBIForWindows"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.MinecraftUWP"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.Office.OneNote"
+    "Microsoft.People"
+    "Microsoft.Print3D"    
+    "Microsoft.WindowsAlarms"
+    # "microsoft.windowscommunicationsapps"        # Mail and Calender     
+    "Microsoft.WindowsMaps"
+    "Microsoft.SkypeApp"
+    "Microsoft.Wallet"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.ZuneVideo"
+    "Microsoft.YourPhone"
+    # "Microsoft.MSPaint"          # Paint & Paint3D
+    # "Microsoft.ZuneMusic"        # New Media Player in Windows
+
+    # Xbox Apps
+    # "Microsoft.Xbox.TCUI"
+    # "Microsoft.XboxApp"
+    # "Microsoft.XboxGameOverlay"
+    # "Microsoft.XboxGamingOverlay"
+    # "Microsoft.XboxIdentityProvider"
+    # "Microsoft.XboxSpeechToTextOverlay"
+
+    # Threshold 2 apps
+    "Microsoft.GetHelp"
+    "Microsoft.Getstarted"
+    "Microsoft.Messaging"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.WindowsFeedbackHub"
+
+    # Creators Update apps
+    "Microsoft.Microsoft3DViewer"
+
+    #Redstone apps
+    "Microsoft.BingFoodAndDrink"
+    "Microsoft.BingHealthAndFitness"
+    "Microsoft.BingTravel"
+    "Microsoft.WindowsReadingList"
+
+    # Redstone 5 apps
+    "Microsoft.MixedReality.Portal"
+    "Microsoft.Whiteboard"
+
+    # non-Microsoft
+    # "4DF9E0F8.Netflix"
+    # "SpotifyAB.SpotifyMusic"
+    "2FE3CB00.PicsArt-PhotoStudio"
+    "46928bounde.EclipseManager"
+    "613EBCEA.PolarrPhotoEditorAcademicEdition"
+    "6Wunderkinder.Wunderlist"
+    "7EE7776C.LinkedInforWindows"
+    "89006A2E.AutodeskSketchBook"
+    "9E2F88E3.Twitter"
+    "A278AB0D.DisneyMagicKingdoms"
+    "A278AB0D.MarchofEmpires"
+    "ActiproSoftwareLLC.562882FEEB491"
+    "CAF9E577.Plex"
+    "ClearChannelRadioDigital.iHeartRadio"
+    "D52A8D61.FarmVille2CountryEscape"
+    "D5EA27B7.Duolingo-LearnLanguagesforFree"
+    "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+    "DolbyLaboratories.DolbyAccess"
+    "DolbyLaboratories.DolbyAccess"
+    "Drawboard.DrawboardPDF"
+    "Facebook.Facebook"
+    "Fitbit.FitbitCoach"
+    "Flipboard.Flipboard"
+    "GAMELOFTSA.Asphalt8Airborne"
+    "KeeperSecurityInc.Keeper"
+    "NORDCURRENT.COOKINGFEVER"
+    "PandoraMediaInc.29680B314EFC2"
+    "Playtika.CaesarsSlotsFreeCasino"
+    "ShazamEntertainmentLtd.Shazam"
+    "SlingTVLLC.SlingTV"
+    "TheNewYorkTimes.NYTCrossword"
+    "ThumbmunkeysLtd.PhototasticCollage"
+    "TuneIn.TuneInRadio"
+    "WinZipComputing.WinZipUniversal"
+    "XINGAG.XING"
+    "flaregamesGmbH.RoyalRevolt2"
+    "king.com.*"
+    "king.com.BubbleWitch3Saga"
+    "king.com.CandyCrushSaga"
+    "king.com.CandyCrushSodaSaga"
+);
+
+
+Write-Host -ForegroundColor Cyan "Remove bloatware"
+Foreach ($blt in $bloatware) {
+    Write-Host -ForegroundColor Red "Removing:" $blt
+    Get-AppxPackage -AllUsers $blt | Remove-AppxPackage
+}
+Pause
+Clear-Host
